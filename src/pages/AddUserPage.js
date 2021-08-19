61b06fc1ee93e565679a858c4e9bac6bd7ba7f7f
liaojiayun <liaojiayun@moresales.cn>
2021-08-19 21:35:25 +0800

    first commit

---
+++ src/pages/AddUserPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AddUsersPage = () => <div>Add Users</div>;
+
+export default AddUsersPage;
---
+++ src/pages/UserProfilePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserProfilePage = ({ match }) => (
+  <div>User Profile for user: {match.params.userId}</div>
+);
+
+export default UserProfilePage;
