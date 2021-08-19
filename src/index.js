03295cefe745626f01872703129ee7cc291004da
liaojiayun <liaojiayun@moresales.cn>
2021-08-19 21:48:43 +0800

    feat: add docs

--- src/index.js
+++ src/index.js
@@ -9,3 +9,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// https://segmentfault.com/a/1190000014691570
+// https://juejin.cn/post/6844903924441284615
+// http://interview.poetries.top/fe-react-docs/react-web-action/07-%E5%AE%9E%E6%88%98%E7%AF%87%2004%EF%BC%9A%E6%9D%83%E9%99%90%E7%AE%A1%E7%90%86%E6%9C%BA%E5%88%B6.html
